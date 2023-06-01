2a7f6ad76640752d6b84ce3573aa9ed734edd7fb
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2023-06-01 18:40:58 +0400

    Fix menu bar on mobile view

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,14 @@
     <nav class="navbar is-dark">
       <div class="navbar-brand">
         <router-link to="/" class="navbar-item"><strong>ecommerce</strong></router-link>
-        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu"
+           v-on:click="showMobileMenu= !showMobileMenu">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
-      <div class="navbar-menu" id="navbar-menu">
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
         <div class="navbar-end">
           <router-link to="/summer" class="navbar-item">Summer</router-link>
           <router-link to="/winter" class="navbar-item">Winter</router-link>
@@ -39,5 +40,13 @@
 <style lang="scss">
 @import "../node_modules/bulma";
 </style>
-<script setup>
+<script>
+export default {
+  data() {
+    return {
+      showMobileMenu: false,
+    }
+  }
+
+}
 </script>
